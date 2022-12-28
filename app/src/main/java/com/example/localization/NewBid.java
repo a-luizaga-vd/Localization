fa3df1657b2433cc16dd4fc77930ded711cdef68
Leandro Luna <LeandroLuna200@users.noreply.github.com>
2022-12-28 15:54:24 -0300

    update bidding

---
+++ app/src/main/java/com/example/localization/NewBid.java
@@ -0,0 +1,22 @@
+package com.example.localization;
+
+public class NewBid {
+    String price;
+    String minutes;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(String minutes) {
+        this.minutes = minutes;
+    }
+}
--- app/src/main/java/com/example/localization/SettingBiddingActivity.java
+++ app/src/main/java/com/example/localization/SettingBiddingActivity.java
@@ -3,6 +3,8 @@ package com.example.localization;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.microsoft.signalr.HubConnection;
@@ -13,6 +15,9 @@ public class SettingBiddingActivity extends AppCompatActivity {
     HubConnection hubConnection;
     String token;
 
+    EditText et_price;
+    EditText et_time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         token = HomeActivity.myToken;
@@ -20,20 +25,41 @@ public class SettingBiddingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_bidding);
 
-        hubConnection = HubConnectionBuilder.create("http://35.239.225.98:443/hubs/")
+        et_price = findViewById(R.id.openingBinding);
+        et_time = findViewById(R.id.limitNumber);
+
+        hubConnection = HubConnectionBuilder.create("http://35.239.225.98:443/hubs/bids")
                 .withHeader("Authorization", "Bearer " + token)
                 .build();
 
         hubConnection.start();
         Toast.makeText(this, "Conexion exitosa", Toast.LENGTH_SHORT).show();
 
+//        hubConnection.send("JoinGroup");
 
+        hubConnection.on("NewUser", (msg) -> {
+            System.out.println("Message: "+(msg));
+        }, String.class);
+
+        hubConnection.on("StartBid", (message) -> {
+            System.out.println("Message: "+(message));
+        }, String.class);
     }
 
     public void onDestroy() {
 
         super.onDestroy();
+        hubConnection.stop();
+        Toast.makeText(this, "Te desconectaste chau...", Toast.LENGTH_SHORT).show();
     }
 
 
+    public void starBidding(View v){
+        hubConnection.send("JoinGroup");
+        //faltan validaciones de los edit text
+        NewBid newBid = new NewBid();
+        newBid.setPrice(et_price.getText().toString());
+        newBid.setMinutes(et_time.getText().toString());
+        hubConnection.send("startBidding", newBid);
+    }
 }
