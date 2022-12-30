9a1e847f5b52c4f621875049efe983b1b822cd3b
Leandro Luna <LeandroLuna200@users.noreply.github.com>
2022-12-30 13:05:51 -0300

    update timer

--- app/src/main/java/com/example/localization/BiddingStatusActivity.kt
+++ app/src/main/java/com/example/localization/BiddingStatusActivity.kt
@@ -7,48 +7,48 @@ import android.widget.Chronometer
 import android.widget.Chronometer.OnChronometerTickListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.log
 
 //import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 
 class BiddingStatusActivity : AppCompatActivity(), OnChronometerTickListener {
 
-    private lateinit var counter : Chronometer
+
+    private lateinit var counter: Chronometer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_state_bidding)
 
-        counter = findViewById( R.id.chronometerBid)
-
         startCounter()
+
     }
 
-    private fun startCounter(){
-        counter.isCountDown = true
-        counter.base = SystemClock.elapsedRealtime() + 10000
-        counter.start()
-
-        counter.setOnChronometerTickListener {
-            this
-        }
-    }
-//    OnChronometerTickListener{
-//        chronometer ->
-//        Log.i("TIMER", chronometer.text.toString())
-//        if (chronometer.text.toString().equals("00:00", ignoreCase = true))
-//            Toast.makeText(
-//                this,
-//                "time reached", Toast.LENGTH_SHORT).show()
-//    }
-
-    override fun onChronometerTick(p0: Chronometer?) {
-        Log.i("TIMER", counter.text.toString())
-        Log.i("TIMER", "CALLED")
-        if (counter.text.toString().equals("00:00", ignoreCase = true))
+    override fun onChronometerTick(chronometer: Chronometer) {
+        Log.e("onChronometerTick", "called")
+        if (chronometer.text.toString().equals("00:00", ignoreCase = true)) {
+            chronometer.stop();
             Toast.makeText(
                 this,
-                "time reached", Toast.LENGTH_SHORT).show()
+                "time reached", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    //
+    private fun startCounter() {
+
+        counter = findViewById(R.id.chronometerBid)
+        counter.isCountDown = true
+
+        //para cambiar el tiempo base
+        counter.base = SystemClock.elapsedRealtime() + 10000
+
+        counter.onChronometerTickListener = this
+//        counter.setOnChronometerTickListener {
+//            this
+//        }
+        counter.start()
+        Log.v("TIMER", "Started")
     }
 }
